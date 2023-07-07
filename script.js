fba7f63d85000b617df11446e49945c37ce0f67a
Jameshuff95 <126839048+Jameshuff95@users.noreply.github.com>
2023-07-07 06:39:11 +0000

    sd

--- script.js
+++ script.js
@@ -4,12 +4,14 @@ window.onload = function() {
   switchSkill('frontend_list');
   switchOtherSkill('technical_skills');
   switchImage('image0_div');
+  flashlight.style.display = 'none';
  };
 
 // themes
 const lightMode = {
   background_color: "white",
-  color: "black"
+  color: "black",
+  color1: "lightgreen"
 };
 
 const darkMode = {
@@ -22,6 +24,7 @@ const audio = document.createElement('audio');
   audio.src = 'https://cdn.freesound.org/previews/263/263994_3076984-lq.mp3';
 const musicOn = document.querySelector('#play');
 const musicOff = document.querySelector('#pause');
+const cv = document.querySelector('#cv');
 const div = document.querySelector('#switch_tip');
 const switcher = document.querySelector('#inner_switch');
 const body = document.querySelector('body');
@@ -30,6 +33,7 @@ const flashlight = document.querySelector('#flashlight');
 const listContainer = document.getElementById('list_container');
 const arrowLeft = document.querySelector('.fa-arrow-left');
 const arrowRight = document.querySelector('.fa-arrow-right');
+const picture = document.querySelector('#my_photo');
 
 // initial states
 let isClicked = false;
@@ -43,11 +47,15 @@ body.style.color = lightMode.color;
 for (let i = 0; i < navLinks.length; i++) {
   navLinks[i].style.color = lightMode.color;
 }
+cv.style.boxShadow = lightMode.color;
+picture.style.borderColor = lightMode.color1;
 
+// This defines the play variable for the fireside sounds
 const play = () => {
   audio.play();
 }
 
+// This defines the pause variable for the fireside sounds
 const pause = () => {
   audio.pause();
 }
@@ -71,8 +79,9 @@ switcher.addEventListener('click', function(event) {
     div.style.transform = `translate(${originalX}px, ${originalY}px)`;
     flashlight.style.display = "none";
     body.style.backgroundColor = lightMode.background_color;
-	  
     body.style.color = lightMode.color;
+    cv.style.boxShadow = lightMode.color1;
+    picture.style.borderColor = lightMode.color1;
     for (let i = 0; i < navLinks.length; i++) {
       navLinks[i].style.color = lightMode.color;
     }
@@ -84,6 +93,8 @@ switcher.addEventListener('click', function(event) {
       flashlight.style.display = "block";
       body.style.backgroundColor = darkMode.background_color;
       body.style.color = darkMode.color;
+      cv.style.boxShadow = darkMode.color;
+      picture.style.borderColor = darkMode.color;
       for (let i = 0; i < navLinks.length; i++) {
         navLinks[i].style.color = darkMode.color;
       }
@@ -96,56 +107,39 @@ function updateFlashlightPosition(x, y) {
   // Get the scroll offset of the page
   const scrollX = window.pageXOffset;
   const scrollY = window.pageYOffset;
-
   // This adds a smooth CSS transition property to the flashlight
   flashlight.style.transition = 'left 0.2s ease, top 0.2s ease';
-
   // This accounts for the scroll offset
   flashlight.style.left = x - 50 + scrollX + 'px';
   flashlight.style.top = y - 50 + scrollY + 'px';
 }
 
+// This gets the cursor coordinates relative to the viewport
 document.addEventListener('mousemove', function(event) {
-  // Get the cursor coordinates relative to the viewport
   const x = event.clientX;
   const y = event.clientY;
-
   updateFlashlightPosition(x, y);
 });
 
 document.addEventListener('touchmove', function(event) {
   // Prevent default touch event behavior
   event.preventDefault();
-
   // Get the first touch point's coordinates relative to the viewport
   const touch = event.touches[0];
   const x = touch.clientX;
   const y = touch.clientY;
-
   updateFlashlightPosition(x, y);
 });
 
-function showIcon(iconId) {
-  let volumeSwitchIcon = document.getElementById(iconId);
-  let icons = document.getElementsByClassName('audio_icon');
-
-  for (let i = 0; i < icons.length; i++) {
-    icons[i].style.display = 'none';
-  }
-
-  volumeSwitchIcon.style.display = 'block';
-}
 
 // This determines what section is displayed connected by onclick used with each navLink in HTML file
 function showSection(sectionId) {
   let sectionToShow = document.getElementById(sectionId);
   let sections = document.getElementsByTagName('section');
-	
   // hide all other sections
   for (let i = 0; i < sections.length; i++) {
     sections[i].style.display = 'none';
   }
-	
   // show the currently selected section
   sectionToShow.style.display = 'block';
 }
@@ -154,12 +148,10 @@ function showSection(sectionId) {
 function switchSkill(divId) {
   let listToShow = document.getElementById(divId);
   let lists = document.getElementsByClassName('list_container_ul');
-	 
-  // hide all other sections
+  // This hides all other sections
   for (let i = 0; i < lists.length; i++) {
     lists[i].style.display = 'none';
   }
-	
   // show the currently selected section
   listToShow.style.display = 'block';
 }
@@ -168,11 +160,9 @@ function switchSkill(divId) {
 function switchOtherSkill(divId) {
   let skillToShow = document.getElementById(divId);
   let skill = document.getElementsByClassName('skill_div');
-	
   for (let i = 0; i < skill.length; i++) {
     skill[i].style.display = 'none';
   }
-	
   skillToShow.style.display = 'block';
 }
 
@@ -180,12 +170,8 @@ function switchOtherSkill(divId) {
 function switchImage(divId) {
   const imageToShow = document.getElementById(divId);
   const image = document.getElementsByClassName('project_container');
-		
   for (let i = 0; i < image.length; i++) {
     image[i].style.display = 'none';
   }
-	
   imageToShow.style.display = 'block';
 }
-
-
